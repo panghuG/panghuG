f489992c414bdbb3731d5216afcde20d8f6be3f3
panghuG <84706441+panghuG@users.noreply.github.com>
2025-01-24 15:04:36 +0800

    Add files via upload

---
+++ pa.js
@@ -0,0 +1,13 @@
+
+import './pa.css'
+
+
+const div = document.createElement('div')
+div.className='header'
+
+
+
+const script =document.createElement('script')
+script.src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js'
+script.type='text/javascript'
+document.head.appendchild(script)
