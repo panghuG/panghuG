09d84f84cfa0aab23f0335354915c9a4b5cfb286
panghuG <84706441+panghuG@users.noreply.github.com>
2025-01-24 15:14:34 +0800

    Update pa.js

--- pa.js
+++ pa.js
@@ -5,9 +5,3 @@ import './pa.css'
 const div = document.createElement('div')
 div.className='header'
 
-
-
-const script =document.createElement('script')
-script.src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js'
-script.type='text/javascript'
-document.head.appendchild(script)
